63770d9fb0746f3f2a7552c6b03aef8cf684956b
rzzz0 <rudyhsurh@gmail.com>
2023-12-04 12:38:24 +0800

    array test

--- src/main/java/com/example/ArrayOperations.java
+++ src/main/java/com/example/ArrayOperations.java
@@ -4,18 +4,35 @@ public class ArrayOperations {
     // Exercise 1: Find the Maximum Value in an Array
     public static int findMaximumValue(int[] array) {
         // TODO: Implement the logic to find the maximum value in the array.
-        return 0; // Placeholder return value
+        int maxNum = 0;
+        for (int i : array){
+            if (i > maxNum) {
+                maxNum = i;
+            }
+         // Placeholder return value
+        }
+        return maxNum;
     }
 
     // Exercise 2: Calculate the Average of Array Elements
     public static double calculateAverage(int[] array) {
         // TODO: Implement the logic to calculate the average of the array elements.
-        return 0.0; // Placeholder return value
+        int sum = 0;
+        for (int i : array){
+            sum += i;
+        }
+        return sum / array.length; // Placeholder return value
     }
 
     // Exercise 3: Reverse an Array
     public static int[] reverseArray(int[] array) {
         // TODO: Implement the logic to reverse the array.
-        return new int[0]; // Placeholder return value
+        for (int i = 0; i < array.length/2; i++) {
+            int temp = array[array.length-1-i];
+            array[i] = temp;
+            array[array.length-1-i] = array[i];
+            
+        }
+        return array; // Placeholder return value
     }
 }
